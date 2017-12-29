2891f233743f2acc9e4e76f32a348397e9427a5f
Julian Berger <jberger95@gmail.com>
2017-12-29 16:27:48 +0100

    New Logo style Added File Selection Link Added About Page Some Design tweaks Reset data on Info Card Close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { Title } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { FileUploader } from 'ng2-file-upload';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-import {NavigationStart, Router} from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
+import { numberToWords } from 'number-to-words';
 
 enum InfoCardContent {
   Closed = 0,
@@ -29,6 +30,7 @@ export class AppComponent {
   public showExif = false;
   public exifData = {};
   public shutterCount = 0;
+  public shutterCountWords = '';
 
   constructor(private router: Router,
               private titleService: Title,
@@ -119,6 +121,8 @@ export class AppComponent {
         }
       }
 
+      this.shutterCount ? this.shutterCountWords = numberToWords.toWords(this.shutterCount).toUpperCase() : this.shutterCountWords = '';
+
       // Format Date/Time
       Object.keys(this.exifData).forEach((key) => {
         if (this.exifData[key] && this.exifData[key]['year']) {
@@ -136,54 +140,16 @@ export class AppComponent {
   }
 
   public toggleInfoCard(content: InfoCardContent) {
+    // Reset data on info card closing
+    if (content === InfoCardContent.Closed) {
+      this.response = '';
+      this.showExif = false;
+      this.exifData = {};
+      this.shutterCount = 0;
+      this.shutterCountWords = '';
+    }
+
     this.infoCardState = content;
-  }
-
-  public toWords(s: any) {
-    const th = ['','thousand','million', 'billion','trillion'];
-    const dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
-    const tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
-    const tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-
-    s = s.toString();
-    s = s.replace(/[\, ]/g, '');
-    if (s != parseFloat(s)) return 'not a number';
-    let x = s.indexOf('.');
-    if (x == -1) x = s.length;
-    if (x > 15) return 'too big';
-    let n = s.split('');
-    let str = '';
-    let sk = 0;
-    for (var i = 0; i < x; i++) {
-      if ((x - i) % 3 == 2) {
-        if (n[i] == '1') {
-          str += tn[Number(n[i + 1])] + ' ';
-          i++;
-          sk = 1;
-        }
-        else if (n[i] != 0) {
-          str += tw[n[i] - 2] + ' ';
-          sk = 1;
-        }
-      }
-      else if (n[i] != 0) {
-        str += dg[n[i]] + ' ';
-        if ((x - i) % 3 == 0) str += 'hundred ';
-        sk = 1;
-      }
-
-
-      if ((x - i) % 3 == 1) {
-        if (sk) str += th[(x - i - 1) / 3] + ' ';
-        sk = 0;
-      }
-    }
-    if (x != s.length) {
-      let y = s.length;
-      str += 'point ';
-      for (let i = x + 1; i < y; i++) str += dg[n[i]] + ' ';
-    }
-    return str.replace(/\s+/g, ' ').toUpperCase();
   }
 
   public formatedExifList(exif: Object) {
